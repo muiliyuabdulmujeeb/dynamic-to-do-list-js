28d5cd1103b813fa73d0940fed239489e3be8c6e
Muiliyu Abdul-mujeeb <Muiliyuabdulmujeeb@gmail.com>
2026-01-31 23:23:51 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const addButton = document.getElementById("add-task-btn");
+    const taskInput = document.getElementById("task-input");
+    const taskList = document.getElementById("task-list");
+
+    function addTask() {
+        const taskText = taskInput.value.trim();
+        if (taskText === "") {
+            alert("Empty task, kindly input a task")
+        } else {
+            const taskTextLi = document.createElement("li");
+            taskTextLi.textContent = taskText;
+
+            const removeBtn = document.createElement("button");
+            removeBtn.classList.add("remove-btn");
+            removeBtn.textContent = "Remove";
+            removeBtn.addEventListener("click", () => {
+                removeBtn.parentElement.remove()
+            });
+
+            taskTextLi.append(removeBtn);
+            taskList.append(taskTextLi);
+            taskInput.value = "";
+
+        };
+
+    };
+
+    addButton.addEventListener("click", addTask);
+    taskInput.addEventListener("keypress", (e) => {
+        if (e.key === "Enter") {
+            addTask()
+        };
+    });
+    
+    document.addEventListener("DOMContentLoaded", addTask);
+});
